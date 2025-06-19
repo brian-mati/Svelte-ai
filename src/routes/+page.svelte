f9eedf09f02cd1eda4335f975d3b551697716e0d
brian-mati <brianmuthomimati@gmail.com>
2025-06-19 12:34:16 +0300

    docs: Added a bit of client side functionality to test responses

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    // import { chatClient } from "$lib/mistral/entry-point";
+    import { Mistral } from '@mistralai/mistralai';
+if (!apiKey) {
+	ReferenceError('Missing key');
+}
+
+const mistral = new Mistral({ apiKey: apiKey });
+
+ async function chatClient(request: string) {
+	const response = await mistral.chat.complete({
+		model: 'mistral-small-latest',
+		messages: [
+			{
+				content: request,
+				role: 'user'
+			}
+		]
+	});
+	console.info(response.choices[0].message.content);
+	return response;
+}
+
+
+    let question = $state('')
+    function onclick(){
+        console.info(question)
+        if (question == ''){
+            return
+        }
+        chatClient(question)
+    }
+
+</script>
+
+
+<section class="w-sm mx-auto mt-8">
+    <input type="text" class="bg-gray-400 w-full mb-4 h-8" bind:value={question}>
+    <button class="bg-black text-white w-full text-center h-8" {onclick}>Submit</button>
+</section>
+
