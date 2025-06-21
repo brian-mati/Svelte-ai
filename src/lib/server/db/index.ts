5efe5fe33cf28593ca4fdcaf98a43adbf30b908d
brian-mati <brianmuthomimati@gmail.com>
2025-06-21 13:31:07 +0300

    adjustment

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,7 +2,6 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { DATABASE_URL } from '$env/static/private';
 
-console.info({ database_url: DATABASE_URL });
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = neon(DATABASE_URL);
