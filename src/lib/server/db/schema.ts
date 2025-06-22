6ff3922def5abe0810d18676d1682dfea0fabc5d
brian-mati <brianmuthomimati@gmail.com>
2025-06-22 16:23:04 +0300

    feat: correct db schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,12 +1,22 @@
 import { pgTable, serial, integer, varchar, timestamp, text } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 
-// export const test = pgTable('test', {
-// 	id: serial('id').primaryKey(),
-// 	age: integer('age')
-// });
+
+export const chat = pgTable('chat', {
+	id: serial('id').primaryKey(),
+	title: text('title').notNull(),
+	created_at: timestamp('created_at').defaultNow().notNull()
+});
+
+//  one chat has many conversations 
+export const chatConversation = relations(chat,({many}) =>({
+	convesations:many(conversation)
+}) )
+// a conversation belongs to one chat
 
 export const conversation = pgTable('conversation', {
 	id: serial('id').primaryKey(),
+	chatId:integer('chat_id').references(() => chat.id , {onDelete:'cascade',onUpdate:'cascade'}).notNull(),
 	question: varchar('question').notNull(),
 	response: text('response').notNull(),
 	prompt_tokens: integer('prompt_tokens').notNull(),
@@ -14,4 +24,15 @@ export const conversation = pgTable('conversation', {
 	created_at: timestamp('created_at').defaultNow().notNull()
 });
 
+export const conversationRelations = relations(conversation,({one}) => ({
+	author:one(
+		chat,{
+			fields:[conversation.chatId],
+			references:[chat.id]
+
+		}
+	)
+}));
+
 export type Conversation = typeof conversation.$inferInsert;
+
